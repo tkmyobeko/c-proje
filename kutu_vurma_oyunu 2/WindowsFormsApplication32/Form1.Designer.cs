37301c232b2592d774a019ae28e6d06137ec0698
tkmyobeko <56281443+tkmyobeko@users.noreply.github.com>
2019-10-08 03:02:38 +0300

    Add files via upload

---
+++ kutu_vurma_oyunu 2/WindowsFormsApplication32/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(773, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 641);
+            this.label1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.pictureBox1.Location = new System.Drawing.Point(382, 576);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 20);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(-1, 631);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(784, 10);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.LimeGreen;
+            this.label3.Location = new System.Drawing.Point(810, 370);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Hazir";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.Color.Black;
+            this.progressBar1.ForeColor = System.Drawing.Color.LimeGreen;
+            this.progressBar1.Location = new System.Drawing.Point(813, 410);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 13);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            this.progressBar1.Value = 100;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.Color.Chartreuse;
+            this.label4.Location = new System.Drawing.Point(810, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Puanınız: 0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label5.Location = new System.Drawing.Point(810, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Zorluk Seviyesi: 0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label6.Location = new System.Drawing.Point(819, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(134, 22);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Kalan Süreniz";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label7.Location = new System.Drawing.Point(853, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 22);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "00:60";
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.ForeColor = System.Drawing.Color.Red;
+            this.label8.Location = new System.Drawing.Point(785, 91);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(197, 10);
+            this.label8.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.BackColor = System.Drawing.Color.White;
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(785, 199);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(197, 10);
+            this.label9.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.button1.Location = new System.Drawing.Point(801, 445);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 23);
+            this.button1.TabIndex = 11;
+            this.button1.TabStop = false;
+            this.button1.Text = "Oyun Ayarları İçin Tıklayınız!";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlText;
+            this.ClientSize = new System.Drawing.Size(983, 642);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Kutu Vurma 2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ kutu_vurma_oyunu 2/WindowsFormsApplication32/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        PictureBox[] pb_hedef = new PictureBox[20];
+        PictureBox[] pb_mermi = new PictureBox[60];
+        Random rnd = new Random();
+        int hiz = 1;
+        int can = 3;
+        int sure = 59;
+        int zorluk_seviyesi = 0;
+        int yon_degis = 0;
+        int zorluk_ayar = 25;
+        int kim = 0;
+        int puan = 0;
+        int hedefe_ulasan_mermi_sirasi = 0;
+        int atilan_mermi_sirasi = -1;
+        int atilan_mermi_adeti = 60;
+        bool doldurma_suresi = true;
+        bool durdurma = true;
+
+        private void pbMermiOlustur()
+        {
+            int aralikx = label3.Location.X + 4;
+            int araliky = 230;
+            for (int i = 0; i < pb_mermi.Length; i++)
+            {
+                pb_mermi[i] = new PictureBox();
+                pb_mermi[i].Name = "picture_box" + (i + 1).ToString();
+                pb_mermi[i].BackColor = Color.Red;
+                pb_mermi[i].Width = 5;//5
+                pb_mermi[i].Height = 30;//30
+                pb_mermi[i].Image = Image.FromFile("res//m1a.png");
+                pb_mermi[i].SizeMode = PictureBoxSizeMode.StretchImage;
+                pb_mermi[i].Top = araliky;
+                pb_mermi[i].Left = aralikx;
+                this.Controls.Add(pb_mermi[i]);
+                araliky += 6;
+                if ((i + 1) % 20 == 0)
+                {
+                    aralikx += 20;
+                    araliky = 230;
+                }
+            }
+        }
+
+        private void mermiKonumlandırma()
+        {//tekrar doldurma sirasinda çalışan kod
+            int aralikx = label3.Location.X + 4;
+            int araliky = 230;
+            for (int i = 0; i < pb_mermi.Length; i++)
+            {
+                pb_mermi[i].Top = araliky;
+                pb_mermi[i].Left = aralikx;
+                pb_mermi[i].Visible = true;
+                pb_mermi[i].Enabled = true;
+                araliky += 6;
+                if ((i + 1) % 20 == 0)
+                {
+                    aralikx += 20;
+                    araliky = 230;
+                }
+            }
+        }
+
+        private void pbOlustur(int bastanUzaklik, int aralikUzakligi)
+        {
+            int aralik = bastanUzaklik;
+            for (int i = 0; i < pb_hedef.Length; i++)
+            {
+                pb_hedef[i] = new PictureBox();
+                pb_hedef[i].Name = "picture_box" + (i + 1).ToString();
+                pb_hedef[i].BackColor = Color.Red;
+                pb_hedef[i].Width = 20;
+                pb_hedef[i].Height = 20;
+                pb_hedef[i].Top = -20;
+                pb_hedef[i].Left = aralik;
+                this.Controls.Add(pb_hedef[i]);
+                aralik += 20 + aralikUzakligi;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pbOlustur(50, 5);
+            pbMermiOlustur();
+            progressBar1.Maximum = pb_mermi.Length;
+            pb_hedef[0].Location = new Point(rnd.Next(0, label1.Location.X - 20), 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            #region kirmizi_kutu_hareketleri_ve_vurulma_kodlari
+            if (pb_hedef[kim].Location.Y >= label2.Location.Y)
+            {
+                kim++;
+                if (kim == 20)
+                {
+                    kim = 0;
+                }
+                pb_hedef[kim].Location = new Point(rnd.Next(0, label1.Location.X - 20), 0);
+                can = 3;
+            }
+            else if (pb_hedef[kim].Top == pictureBox1.Top + 20)
+            {
+                puan -= 10;
+                pb_hedef[kim].Top += hiz;
+            }
+            else
+            {
+                pb_hedef[kim].Top += hiz;
+                pb_hedef[kim].Left += yon_degis;
+                for (int i = 0; i <= atilan_mermi_sirasi; i++)
+                {
+                    if (pb_hedef[kim].Location.X + 20 >= pb_mermi[i].Location.X && pb_hedef[kim].Location.Y <= pb_mermi[i].Location.Y && pb_hedef[kim].Location.Y + pb_hedef[kim].Height >= pb_mermi[i].Location.Y && pb_mermi[i].Location.X + pb_mermi[i].Width >= pb_hedef[kim].Location.X && pb_mermi[i].Enabled)
+                    {//hedef vurulunca yapılması gereken kodlar
+                        can--;
+                        if (can == 0)
+                        {
+                            pb_hedef[kim].Top = label2.Top;
+                            puan += 10 + zorluk_seviyesi;
+                            if (puan > zorluk_ayar)
+                            {
+                                if (zorluk_seviyesi == 6)
+                                {
+                                    zorluk_ayar += 50 + zorluk_seviyesi;
+                                }
+                                else
+                                {
+                                    zorluk_seviyesi++;
+                                }
+
+                            }
+                        }
+                        if (zorluk_seviyesi > 0 && zorluk_seviyesi < 6)
+                        {
+                            if (zorluk_seviyesi == 5)
+                            {
+                                hiz = 2;
+                            }
+                            if (yon_degis > 0)
+                            {
+                                yon_degis = zorluk_seviyesi * -1;
+                            }
+                            else
+                            {
+                                yon_degis = zorluk_seviyesi * -1;
+                            }
+                        }
+                        pb_mermi[i].Enabled = false;
+                        pb_mermi[i].Visible = false;
+                    }
+                    if (pb_mermi[i].Location.Y == 0 && pb_mermi[i].Enabled)
+                    {//ıskalayınca yapılması gerekenler
+                        puan -= 1;
+                    }
+                }
+            }
+            #endregion
+            #region kirmizi_kutu_icin_sinir_belirleme
+            if (pb_hedef[kim].Location.X <= 0)
+            {
+                yon_degis *= -1;
+            }
+            else if (pb_hedef[kim].Location.X + pb_hedef[kim].Width >= label1.Location.X)
+            {
+                yon_degis *= -1;
+            }
+            #endregion
+            #region mermi_hareketleri
+            for (int i = hedefe_ulasan_mermi_sirasi; i <= atilan_mermi_sirasi; i++)
+            {
+                pb_mermi[i].Top -= 10;
+                if (pb_mermi[i].Location.Y <= -10)
+                {
+                    hedefe_ulasan_mermi_sirasi++;
+                }
+            }
+            #endregion
+            #region mavi_kutu_hareketleri
+            if (MousePosition.X - 15 <= this.Location.X)
+            {
+                pictureBox1.Location = new Point(0, pictureBox1.Location.Y);
+            }
+            else if (MousePosition.X >= this.Location.X + label1.Location.X - 5)
+            {
+                pictureBox1.Location = new Point(label1.Location.X - 20, pictureBox1.Location.Y);
+            }
+            else
+            {
+                pictureBox1.Location = new Point(MousePosition.X - this.Location.X - 14, pictureBox1.Location.Y);
+            }
+            #endregion
+            #region progessBar_ayarlama
+            if (doldurma_suresi)
+            {
+                progressBar1.Value = atilan_mermi_adeti * progressBar1.Step;
+            }
+            #endregion
+            #region mermi_doldurma
+            if (!doldurma_suresi)
+            {
+                if (progressBar1.Value == pb_mermi.Length)
+                {//mermi doldurma
+
+                    doldurma_suresi = true;
+                    atilan_mermi_sirasi = -1;
+                    hedefe_ulasan_mermi_sirasi = 0;
+                    label3.ForeColor = Color.LimeGreen;
+                    label3.Text = "Hazir!";
+                    atilan_mermi_adeti = pb_mermi.Length;
+                    mermiKonumlandırma();
+                }
+                else
+                {
+                    progressBar1.Value += progressBar1.Step;
+                }
+            }
+            #endregion
+            //Cursor.Position = new Point(MousePosition.X, pb_hedef[kim].Location.Y + this.Location.Y + 25);
+            //label4.Text = pb_mermi[i].Location.Y.ToString(); mermi doldurma hızı ayarlaması için kullan
+            label4.Text = "Puanınız: " + puan.ToString();
+            label5.Text = "Zorluk seviyesi: " + zorluk_seviyesi.ToString();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (durdurma)
+            {
+                if (doldurma_suresi)
+                {//doldururken ateş etmeyi engeller
+                    if (Keys.Space == e.KeyCode)
+                    {//mermi ateşleme
+                        if (atilan_mermi_sirasi == pb_mermi.Length - 1)
+                        {
+                            label3.ForeColor = Color.Red;
+                            label3.Text = "Doldur!\n(R tuşuna basınız)";
+                        }
+                        else
+                        {//atilan mermi sayasi 19 olana dek çalışan kod ve merminin ilk konumlandırma kısmı
+                            atilan_mermi_adeti--;//progessbar için oluşturulan değişken
+                            atilan_mermi_sirasi++;
+                            pb_mermi[atilan_mermi_sirasi].Location = new Point(pictureBox1.Location.X + 8, pictureBox1.Location.Y);
+                        }
+                    }
+                }
+                if (Keys.R == e.KeyCode)
+                {//mermi doldurma
+                    if (atilan_mermi_adeti != pb_mermi.Length)//mermi doluyken tekrar doldurmayı önler
+                    {
+                        doldurma_suresi = false;// mermi dolumu yaparken atşlemeyi engeller
+                        progressBar1.Value = 0;
+                        atilan_mermi_adeti = pb_mermi.Length;
+                        label3.Text = "Dolduruluyor!";
+                        label3.ForeColor = Color.Gold;
+                    }
+                }
+            }
+            if (Keys.P == e.KeyCode)
+            {
+                if (timer1.Enabled == true)
+                {
+                    timer1.Enabled = false;
+                    timer2.Enabled = false;
+                    durdurma = false;
+                }
+                else
+                {
+                    timer1.Enabled = true;
+                    timer2.Enabled = true;
+                    durdurma = true;
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            label6.Text = "Kalan Süreniz";
+            label7.Text = "00:" + sure.ToString();
+            if (sure == 0)
+            {
+                timer1.Enabled = false;
+                timer2.Enabled = false;
+                MessageBox.Show("Süreniz bitmiştir. Puannınız : " + puan.ToString(), "Oyun bitti!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            sure--;
+        }
+    }
+}
